4892875ac31346b17e8accff496a3473cc526185
JMarcosSF <jamlmarcos@gmail.com>
2015-03-23 23:58:05 -0700

    updated AJAX region map

--- js/script.js
+++ js/script.js
@@ -2,23 +2,26 @@
 
      // Create the module and name it myApp
 	var myApp = angular.module('myApp', []);
+
+	myApp.config(['$httpProvider', function ($httpProvider) {
+  		$httpProvider.defaults.useXDomain = true;
+	}]);
      
      // Create the controller and inject Angular's $scope
 	myApp.controller('mainController', function($scope, $http) {
 		$scope.message = "Transactions By Originating Country";
 
 		$scope.$on('$viewContentLoaded', function(){
-			//TODO CALL skybabble HERE TO LOAD MAP!!!!
-//			$http.get('testCountryMessageCount.json').
-//			$http.get('localhost:8080/messages/countries/messages/count').
-//		    	success(function(data, status, headers, config) {
-//		      	$scope.posts = data;
-//		      	alert(data);
-//		    }).
-//		    error(function(data, status, headers, config) {
-//		      alert("FAILED");
-//		    });
-			drawRegionsMap();
+			var posts;
+			$http.get('http://localhost:8080/messages/countries/messages/count').
+		    	success(function(data, status, headers, config) {
+		    	var arr = data;
+		      	drawRegionsMap(arr);
+		    }).
+		    error(function(data, status, headers, config) {
+		      console.log(data);
+		    });
+			
 		});
 		
 	});
